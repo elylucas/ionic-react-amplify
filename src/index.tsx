1a1c28d145f28f3e5de9c98238d4012d2aca8c6b
Ely Lucas <ely@meta-tek.net>
2020-08-12 16:25:36 -0600

    adding auth

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify'
+import config from './aws-exports';
+Amplify.configure(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 const Tab1: React.FC = () => {
   return (
@@ -17,10 +17,11 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div>We have auth!</div>
+        <AmplifySignOut />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab1;
+export default withAuthenticator(Tab1);
